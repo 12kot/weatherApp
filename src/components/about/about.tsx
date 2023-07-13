138b7ae695cf07a0c9ae287cb068f96dd0d56f45
Nikita Yakoltsevich <Art.artem.nik@gmail.com>
2023-07-13 23:38:24 +0300

    rebuild main info

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import styles from "./about.module.scss";
 
 const About = () => {
-  return <div className={styles.container} id="about">LALALALAL</div>;
+  return (
+    <div className={styles.container} id="about">
+
+    </div>
+  );
 };
 
 export default About;
--- src/components/currentDay/information/information.tsx
+++ src/components/currentDay/information/information.tsx
@@ -1,5 +1,4 @@
 import React, { ReactElement } from "react";
-import SunColorSVG from "ui/colorSVG/sunColor";
 import { useAppSelector } from "hooks/hooks";
 import styles from "./information.module.scss";
 
@@ -9,25 +8,30 @@ const Information = (props: {
   const { name, localtime } = useAppSelector(
     (state) => state.app.weather.currentWeather.location
   );
-  const { temp_c } = useAppSelector((state) => state.app.weather.currentWeather.current);
-  const { text } = useAppSelector(
+  const { temp_c } = useAppSelector(
+    (state) => state.app.weather.currentWeather.current
+  );
+  const { text, icon } = useAppSelector(
     (state) => state.app.weather.currentWeather.current.condition
   );
 
   return (
     <div className={styles.information}>
-      <p className={styles.celsius}>{temp_c}</p>
       <div className={styles.block_info}>
-        <div className={styles.first}>
-          <p>{name}</p>
-          <p className={styles.block_item}>{props.getDate(localtime)}</p>
+
+        <div className={styles.inf}>
+          <p className={styles.celsius}>{temp_c}</p>
+          <div className={styles.first}>
+            <p>{name}</p>
+            <p className={styles.block_item}>{props.getDate(localtime)}</p>
+          </div>
         </div>
+
         <div className={styles.second}>
-           <span className={styles.img}>
-            <SunColorSVG />
-          </span> 
+          <img src={icon} className={styles.img} alt="icon" />
           <p className={styles.block_item}>{text}</p>
         </div>
+        
       </div>
     </div>
   );
